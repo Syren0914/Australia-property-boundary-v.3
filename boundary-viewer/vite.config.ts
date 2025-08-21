f9c432b4c3df2609d58d11e21c4489836fbfe52f
Kira <78575847+Syren0914@users.noreply.github.com>
2025-08-21 07:29:27 -0400

    update

--- boundary-viewer/vite.config.ts
+++ boundary-viewer/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       '/arcgis':{
         target:'https://spatial-img.information.qld.gov.au',
         changeOrigin:true,
-        secure:true,
+        rewrite: p => p.replace(/^\/arcgis/, ''),
       }
     }
   },
