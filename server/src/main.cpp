d11c184ab349d550f5ac1aa68defdfbb07c11740
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2025-09-26 23:43:52 -0400

    OpenMP test

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,8 +1,19 @@
-#include <stdio.h>
+#include <cstdio>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 
 int main()
 {
-    printf("Hello World");
+    #pragma omp parallel for
+    for (int i=0; i<8; ++i) {
+#ifdef _OPENMP
+      std::printf("Hello from %d\n", omp_get_thread_num());
+#else
+      std::printf("Hello (no OpenMP)\n");
+#endif
+    }
 
     return 0;
 }
