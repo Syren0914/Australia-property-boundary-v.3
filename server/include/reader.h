68b542cf12db8d6905557d70689fbcffbd4bb219
Diego Fonseca <fonseca.diego.feeshy@gmail.com>
2025-09-27 07:38:51 -0400

    Better Array

--- server/include/reader.h
+++ server/include/reader.h
@@ -4,8 +4,16 @@
 #include <mutex>
 #include "AABB.hpp"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* @param N: Number of files
  * @param files: Array of file paths
  * @param threads: Number of threads to use (default: 4)
  */
-void init_reader_meters(int N, const char** files, int threads);
+void init_reader_meters(int N, const char* files[N], int threads);
+
+#ifdef __cplusplus
+}
+#endif
